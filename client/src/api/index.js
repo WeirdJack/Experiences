9937ddc1d3e7d3da5ef5177426ae70fddb1a5a4a
WeirdJackgit config --global user.email 17jack92@gmail.com <32279932+WeirdJack@users.noreply.github.com>
2021-06-09 00:47:28 -0400

    adding server url to index.js file in /api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://heroku:5000/'});
+const API = axios.create({ baseURL: 'https://xperiences.herokuapp.com/'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
